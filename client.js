1fcc55817cd8457276de683cd6963dcd90d2e44b
Katie Liu <katieliu59@gmail.com>
2023-01-18 15:09:32 -0500

    Make new client file to hold the function that allows us to handle our TCP connection,remove this code from play.js and use export/require functions to access what we need, add function to tell me when successfully connected to the game server and also to write my name on the server once connected

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+const connect = function () {
+	const conn = net.createConnection({
+		host: "localhost",
+		port: 50541,
+	});
+
+	// interpret incoming data as text
+	conn.setEncoding("utf8");
+
+	conn.on("data", (data) => {
+		console.log("Kicked you cuz you idled", data);
+	});
+
+	conn.on("connect", () => {
+		console.log("Successfully connected to game server");
+		conn.write("Name: JXL");
+	});
+
+	return conn;
+};
+
+module.exports = { connect };
